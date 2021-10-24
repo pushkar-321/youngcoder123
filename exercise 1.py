60ad66e02b36374a7d4e861547a2dbc4ea66c737
youngcoder123 <93084177+youngcoder123@users.noreply.github.com>
2021-10-24 19:05:00 +0530

    Add files via upload

---
+++ exercise 1.py
@@ -0,0 +1,11 @@
+dictionary = { "Acceleration":"Rate of change of velocity",
+               "Speed":"Distance per unit time",
+               "Velocity":"Displacement per unit time",
+               "Distance":"Length of the path travelled" }
+
+user = input("Enter the word you want to know \n ")
+print(dictionary[user])
+
+
+
+
